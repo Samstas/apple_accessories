3844a443ada3fe112ccf8d8e845f1b47cced4e5e
Stas <ssfrontendd@gmail.com>
2024-02-23 12:15:44 +0100

    fix bug with search field outside of main page

--- src/components/SearchField.jsx
+++ src/components/SearchField.jsx
@@ -1,32 +1,34 @@
-import { useState } from "react";
-import { BiSearch } from "react-icons/bi";
-import { useSearchParams } from "react-router-dom";
+import { useState } from 'react'
+import { BiSearch } from 'react-icons/bi'
+import { useSearchParams, useNavigate } from 'react-router-dom'
 
 function SearchField() {
-  const [searchParams, setSearchParams] = useSearchParams();
-  const [searchField, setSearchField] = useState("");
+  const [searchParams, setSearchParams] = useSearchParams()
+  const [searchField, setSearchField] = useState('')
+  const navigate = useNavigate()
 
   function handleSearch() {
-    searchParams.set("query", searchField);
-    setSearchParams(searchParams, { replace: true });
+    searchParams.set('query', searchField)
+    setSearchParams(searchParams, { replace: true })
+    navigate(`/accessories?query=${searchField}`)
   }
 
   return (
     <form
       onSubmit={(e) => {
-        e.preventDefault();
-        handleSearch();
+        e.preventDefault()
+        handleSearch()
       }}
     >
       <label className="relative block">
         <span
           onClick={handleSearch}
-          className="absolute top-[6px] left-2 cursor-pointer text-zinc-600"
+          className="absolute left-2 top-[6px] cursor-pointer text-zinc-600"
         >
-          <BiSearch className="text-zinc-400 text-[1.3rem]"/>
+          <BiSearch className="text-[1.3rem] text-zinc-400" />
         </span>
         <input
-          className=" w-[180px] sm:w-full placeholder-zinc-400 bg-zinc-200 rounded-md py-1 pl-8 pr-8 focus:outline-zinc-600 "
+          className=" w-[180px] rounded-md bg-zinc-200 py-1 pl-8 pr-8 placeholder-zinc-400 focus:outline-zinc-600 sm:w-full "
           placeholder="Search"
           type="text"
           value={searchField}
@@ -34,15 +36,15 @@ function SearchField() {
         />
         {searchField ? (
           <span
-            onClick={() => setSearchField("")}
-            className="absolute inset-y-0 right-0 flex items-center pr-3 cursor-pointer text-zinc-700  hover:text-red-600 transition ease-in"
+            onClick={() => setSearchField('')}
+            className="absolute inset-y-0 right-0 flex cursor-pointer items-center pr-3 text-zinc-700  transition ease-in hover:text-red-600"
           >
             &times;
           </span>
         ) : null}
       </label>
     </form>
-  );
+  )
 }
 
-export default SearchField;
+export default SearchField
--- src/hooks/useFilteredData.js
+++ src/hooks/useFilteredData.js
@@ -1,89 +1,96 @@
-import { useSearchParams } from "react-router-dom";
-import { accessories } from "../data/data-accessories";
+import { useSearchParams } from 'react-router-dom'
+import { accessories } from '../data/data-accessories'
 
 export function useFilteredData() {
-  const data = accessories;
-  const [searchParams] = useSearchParams();
+  const data = accessories
+  const [searchParams] = useSearchParams()
 
   // FILTER accessories
-  const searchQuery = searchParams.get("query");
-  const filterValue = searchParams.get("category");
+  const searchQuery = searchParams.get('query')
+  const filterValue = searchParams.get('category')
 
-  let filteredProducts;
+  let filteredProducts
+
+  // SEARCH Data
+  if (!filterValue) {
+    filteredProducts = data.filter((item) =>
+      item.title.toLowerCase().includes(searchQuery.toLowerCase()),
+    )
+  }
 
   // ALL
-  if (filterValue === "all") {
+  if (filterValue === 'all') {
     filteredProducts = !searchQuery
       ? data
       : data.filter((item) =>
-          item.title.toLowerCase().includes(searchQuery.toLowerCase())
-        );
+          item.title.toLowerCase().includes(searchQuery.toLowerCase()),
+        )
   }
 
   // CASES
-  if (filterValue === "cases") {
+  if (filterValue === 'cases') {
     filteredProducts = !searchQuery
-      ? data.filter((item) => item.category === "cases")
+      ? data.filter((item) => item.category === 'cases')
       : data
-          .filter((item) => item.category === "cases")
+          .filter((item) => item.category === 'cases')
           .filter((item) =>
-            item.title.toLowerCase().includes(searchQuery.toLowerCase())
-          );
+            item.title.toLowerCase().includes(searchQuery.toLowerCase()),
+          )
   }
 
   // BANDS
-  if (filterValue === "bands") {
+  if (filterValue === 'bands') {
     filteredProducts = !searchQuery
-      ? data.filter((item) => item.category === "bands")
+      ? data.filter((item) => item.category === 'bands')
       : data
-          .filter((item) => item.category === "bands")
+          .filter((item) => item.category === 'bands')
           .filter((item) =>
-            item.title.toLowerCase().includes(searchQuery.toLowerCase())
-          );
+            item.title.toLowerCase().includes(searchQuery.toLowerCase()),
+          )
   }
 
   // CHARGERS
-  if (filterValue === "chargers") {
+  if (filterValue === 'chargers') {
     filteredProducts = !searchQuery
-      ? data.filter((item) => item.category === "chargers")
+      ? data.filter((item) => item.category === 'chargers')
       : data
-          .filter((item) => item.category === "chargers")
+          .filter((item) => item.category === 'chargers')
           .filter((item) =>
-            item.title.toLowerCase().includes(searchQuery.toLowerCase())
-          );
+            item.title.toLowerCase().includes(searchQuery.toLowerCase()),
+          )
   }
 
   // CABLES
-  if (filterValue === "cables") {
+  if (filterValue === 'cables') {
     filteredProducts = !searchQuery
-      ? data.filter((item) => item.category === "cables")
+      ? data.filter((item) => item.category === 'cables')
       : data
-          .filter((item) => item.category === "cables")
+          .filter((item) => item.category === 'cables')
           .filter((item) =>
-            item.title.toLowerCase().includes(searchQuery.toLowerCase())
-          );
+            item.title.toLowerCase().includes(searchQuery.toLowerCase()),
+          )
   }
 
   // SORT accessories
-  const sortBy = searchParams.get("sortBy") || "id-asc";
-  const [field, direction] = sortBy.split("-");
+  const sortBy = searchParams.get('sortBy') || 'id-asc'
+  const [field, direction] = sortBy.split('-')
 
   const sortedProducts = filteredProducts?.sort((a, b) => {
-    const modifier = direction === "asc" ? 1 : -1;
+    const modifier = direction === 'asc' ? 1 : -1
 
-    if (field === "id") {
-      return (a.id - b.id) * modifier;
+    if (field === 'id') {
+      return (a.id - b.id) * modifier
     }
 
-    if (field === "price") {
-      return Number(a.price - b.price) * modifier;
+    if (field === 'price') {
+      return Number(a.price - b.price) * modifier
     }
 
-    if (field === "title") {
-      return a.title.localeCompare(b.title) * modifier;
+    if (field === 'title') {
+      return a.title.localeCompare(b.title) * modifier
     }
-    return 0;
-  });
+    return 0
+  })
 
-  return { sortedProducts };
+  return { sortedProducts, filterValue }
 }
